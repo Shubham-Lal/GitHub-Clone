b195c23e3eab95a1b34d7641acbe3b1085c71f72
Shubham Lal <im.shubhamlal@gmail.com>
2023-11-19 22:49:04 +0530

    client: wip Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages'
 
 function App() {
 
   return (
-    <>
-      Let’s build from here
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
