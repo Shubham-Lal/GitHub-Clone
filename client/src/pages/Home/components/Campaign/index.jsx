aeb9861f2cd73bb75fbf894c0df28441268ecd65
Shubham Lal <im.shubhamlal@gmail.com>
2023-11-22 14:24:32 +0530

    client: wip Campaign component

--- client/src/pages/Home/components/Campaign/index.jsx
+++ client/src/pages/Home/components/Campaign/index.jsx
@@ -3,10 +3,34 @@ import './style.css'
 
 const Campaign = () => {
     return (
-        <div className='campaign__container'>
-            <img className='campaign__image-medium' src='/images/campaign-medium.webp' width='860' height='544' alt='Campaign Medium' />
-            <img className='campaign__image-large' src='/images/campaign-large.webp' width='2236' height='1630' alt='Campaign Desktop' />
-        </div>
+        <>
+            <div className='campaign__image'>
+                <img className='campaign__image-medium' src='/images/campaign-medium.webp' width='860' height='544' alt='Campaign Medium' />
+                <img className='campaign__image-large' src='/images/campaign-large.webp' width='2236' height='1630' alt='Campaign Desktop' />
+            </div>
+            <div style={{ padding: '0 16px' }}>
+                <div className='campaign__container'>
+                    <div className='campaign__wrapper'>
+                        <div className='campaign__left'>
+                            <div className='circle__container'>
+                                <div className='circle__dot' />
+                            </div>
+                            <div className='line first' />
+                            <div className='bracket__container'>
+                                <div className='bracket__wrapper'>
+                                    <img src='/images/bracket.svg' alt='Bracket' />
+                                    <div className='bracket__shadow' />
+                                </div>
+                            </div>
+                            <div className='line second' />
+                        </div>
+                        <div className='campaign__right'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
