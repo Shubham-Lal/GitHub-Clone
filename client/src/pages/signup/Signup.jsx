e98e184f0ea0614479b7f1d28753917e9b27e7e5
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-03 03:48:56 +0530

    client: using svh instead of px

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import useWindowHeight from '../../hooks/useWindowHeight'
 import Background from '../../components/Background';
 import BasicNavbar from '../../components/Navbar/BasicNavbar';
 import Form from './components/Form';
 import { SignupFooter } from './components/Footer';
 
 const Signup = () => {
-  const { height, isReady } = useWindowHeight();
-
   return (
     <div
       style={{
         position: 'relative',
         background: '#040d21',
-        minHeight: `${height}px`,
-        opacity: isReady ? 1 : 0,
-        transition: 'opacity 0.5s linear',
+        minHeight: '100svh',
         overflow: 'clip',
         display: 'flex',
         flexDirection: 'column',
